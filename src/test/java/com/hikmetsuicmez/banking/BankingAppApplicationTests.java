ebe760c91abcfa9c89a9b9182201287b412ab7fb
Hikmet Suicmez <hikmet.suicmez1@gmail.com>
2024-02-10 23:05:47 +0300

    created spring boot project

---
+++ src/test/java/com/hikmetsuicmez/banking/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hikmetsuicmez.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
